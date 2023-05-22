6cf6ba787da51d7e59a0abc9c91cfe376c68ab13
Karen Carvalho <karen_carvalho04@hotmail.com>
2023-05-22 15:32:55 -0300

    Sistema model-view-controller

--- src/br/sc/senac/mca/controller/LoginController.java
+++ src/br/sc/senac/mca/controller/LoginController.java
@@ -1,4 +1,15 @@
 package br.sc.senac.mca.controller;
 
+import br.sc.senac.mca.dao.LoginDao;
+import br.sc.senac.mca.model.Login;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class LoginController {
+
+    public ArrayList<Login>buscarLoginSenha()
+            throws SQLException, ClassNotFoundException{
+        return LoginDao.buscarLogin();
+    }
 }
--- src/br/sc/senac/mca/view/LoginView.java
+++ src/br/sc/senac/mca/view/LoginView.java
@@ -1,13 +1,75 @@
 package br.sc.senac.mca.view;
 
-import javax.swing.*;
+import br.sc.senac.mca.controller.LoginController;
+import br.sc.senac.mca.model.Login;
 
-public class LoginView {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static javax.swing.JFrame.*;
+
+public class LoginView extends JFrame {
+    private JPanel panel;
+    private JLabel lblUsuario;
+    private JLabel lblSenha;
+
+
     private JTextField txtLogin;
     private JPasswordField txtSenha;
     private JButton btnLimpar;
     private JButton btnLogin;
-    private JLabel lblUsuario;
-    private JLabel lblSenha;
-    private JPanel pnlLoginView;
+
+    public LoginView() {
+        initComponents();
+        listeners();
+    }
+    public void initComponents() {
+        setTitle("Tela de login");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(panel);
+        setVisible(true);
+    }
+    public void listeners() {
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String login = txtLogin.getText();
+                char[] senha = txtSenha.getPassword();
+
+                try {
+                    LoginController lc = new LoginController();
+                    ArrayList<Login> loginList = lc.buscarLoginSenha();
+
+                    for (Login l : loginList) {
+                        if (l.getLogin().equalsIgnoreCase(login) &&
+                                l.getSenha().equalsIgnoreCase(new String(senha))) {
+                            System.out.println("Login validado");
+
+                            SystemView s = new SystemView();
+                            dispose();
+
+                        } else {
+                            JOptionPane.showMessageDialog(null,
+                                    "Verifique usuário e senha",
+                                    "Erro",
+                                    JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Classe não encontrada");
+                }
+            }
+        });
+    }
+    public static void main(String[] args) {
+        LoginView lv = new LoginView();
+    }
 }
